9b183c977374da1e950ba47ea90d5c5022879fd7
tylerhadley <hadley.tyler@gmail.com>
2015-08-07 16:30:27 -0700

    update postgres module to return sql

--- cursesPostgres.py
+++ cursesPostgres.py
@@ -1,27 +1,49 @@
 #test script to connect to postgres db
 #need to execute as the user that has permission for the db
+#installation instructions for postgres and world db:
+#	sudo apt-get install postgresql postgresql-contrib
+#	sudo -u postgres psql postgres
+#	download world sample database from http://pgfoundry.org/projects/dbsamples/
+#	navigate to world.sql
+#	sudo -u postgres createdb world
+#	sudo -u postgres psql -f world.sql -d world
 
 import psycopg2
 import sys
+import json
 
+#input: json strong with username, database, password(optional), query
+#output: results of query, or False if error. 
+def queryPostgres(json_login):
+	con = None
+	json_login_parsed = json.loads(json_login)
 
-con = None
+	try:
+	    	con = psycopg2.connect(database=json_login_parsed['database'], user=json_login_parsed['username'], password=json_login_parsed['password']) 
+	    	cur = con.cursor() #cursor can be used to execute SQL
+	        if json_login_parsed['query_string']:
+			cur.execute(json_login_parsed['query_string'])
+			return cur.fetchall()       
+	except psycopg2.DatabaseError, e:
+	    	print 'Error %s' % e    
+	    	return False
+	       
+	finally:
+	    	if con:
+			con.close()
 
-try:
-     
-    con = psycopg2.connect(database='mydb', user='postgres') 
-    cur = con.cursor() #cursor can be used to execute SQL
-    cur.execute('SELECT version()')          
-    q = cur.fetchone()
-    print q    
-    
+print "Should be success:"
+json_to_pass = '{ "username" : "postgres", "password": "", "database": "world", "query_string": "SELECT * FROM COUNTRY" }'
+print queryPostgres(json_to_pass)
 
-except psycopg2.DatabaseError, e:
-    print 'Error %s' % e    
-    sys.exit(1)
-    
-    
-finally:
-    
-    if con:
-        con.close()
+print "Should fail:"
+json_to_pass = '{ "username" : "postgres", "password": "", "database": "world", "query_string": "SELECT * FROM COUNTR" }'
+print queryPostgres(json_to_pass)
+
+print "Should fail:"
+json_to_pass = '{ "username" : "postgres", "password": "", "database": "worl", "query_string": "SELECT * FROM COUNTRY" }'
+print queryPostgres(json_to_pass)
+
+print "Should fail:"
+json_to_pass = '{ "username" : "postgre", "password": "", "database": "world", "query_string": "SELECT * FROM COUNTRY" }'
+print queryPostgres(json_to_pass)
